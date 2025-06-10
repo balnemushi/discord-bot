1c7ca6462799fb7378ddfb16d8a8d5a918c32527
balnemushi <sgjunho@gmail.com>
2025-06-10 22:09:03 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f"✅ 봇이 로그인되었습니다! {bot.user}")
+
+@bot.command()
+async def 안녕(ctx):
+    await ctx.send("안녕하세요, 저는 봇입니다! 🤖")
+
+bot.run(os.environ["DISCORD_TOKEN"])
